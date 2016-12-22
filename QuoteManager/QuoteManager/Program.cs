260c47153a31745e20ddb8ed7c672c24c681f2aa
hero24 <hero24@interia.pl>
2016-12-22 17:49:17 +0000

    adding quote dsa

---
+++ QuoteManager/QuoteManager/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace QuoteManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("hello");
+            Quote q = new Quote("Cezary Pazura", "Kobieta u mojego boku musi byc swiatowym topem");
+            //Console.Write(q.quote);
+            Console.Read();
+        }
+    }
+}
---
+++ QuoteManager/QuoteManager/Quote.cs
@@ -0,0 +1,14 @@
+﻿namespace QuoteManager
+{
+    public class Quote
+    {
+        private string author;
+        private string quote;
+        private string[] references;
+        public Quote(string author, string quote)
+        {
+            this.author = author;
+            this.quote = quote;
+        }
+    }
+}

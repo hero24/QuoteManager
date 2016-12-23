1090cfe5e2b1a17264bfa1ae0f4d6e21105ed069
hero24 <hero24@interia.pl>
2016-12-23 18:50:55 +0000

    quote edits

--- Program.cs
+++ Program.cs
@@ -4,11 +4,22 @@ namespace QuoteManager
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.Write("hello");
-            Quote q = new Quote("Cezary Pazura", "Kobieta u mojego boku musi byc swiatowym topem");
-            //Console.Write(q.quote);
-            Console.Read();
+        { 
+            Quote[] quotes = new Quote[100];
+            int i = 0;
+            quotes[i++] = new Quote("Athas","Linux printing was designed and implemented by people working to preserve the rainforest by making it utterly impossible to consume paper.");
+            quotes[i++] = new Quote("Unknown","ALSA is like the emperors new clothes.It never works, but people say it’s because you’re a noob.");
+            quotes[i++] = new Quote("--kfx","ruby is what happens when some kid learns java then looks at perl and thinks ‘I can fix this!’.");
+            for (int j = 0; j < i; j++)
+            {
+                Console.WriteLine(quotes[j].getQuote());
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(quotes[j].author);
+                Console.ResetColor();
+                Console.Read();
+            }
+            
         }
     }
 }
--- Quote.cs
+++ Quote.cs
@@ -2,13 +2,28 @@
 {
     public class Quote
     {
-        private string author;
+        public string author
+        {
+            get { return _author; }
+        }
+        private string _author;
         private string quote;
+        private int i;
         private string[] references;
         public Quote(string author, string quote)
         {
-            this.author = author;
+            this._author = author;
             this.quote = quote;
+            references = new string[10];
+            i = 0;
+        }
+        public string getQuote()
+        {
+            return quote;
+        }
+        public void addReference(string reference)
+        {
+            references[i++] = reference; 
         }
     }
 }

1b359aa57a94fcd7f110c09f476a56922ef25446
hero24 <hero24@interia.pl>
2017-01-20 21:33:52 +0000

    prep for references & flags

--- Quote.cs
+++ Quote.cs
@@ -12,12 +12,19 @@ namespace QuoteManager
         private string _author;
         private string quote;
         private int i;
+        /*
+        TODO:
+        references -> what the quote is about
+        flags -> any user set flags such as, already used.
+        */
         private string[] references;
+        private string[] flags;
         public Quote(string author, string quote)
         {
             this._author = author;
             this.quote = quote;
             references = new string[10];
+            flags = new string[10];
             i = 0;
         }
         public string getQuote()
